16e99d12250375f80892f4761cae3748d20f2732
municorn <municorn@musicaloft.com>
2020-07-07 13:43:23 -0500

    show disconnected message when client loses connection

--- src/bin/muse-status.rs
+++ src/bin/muse-status.rs
@@ -35,15 +35,13 @@ fn main() {
     stream.write_all(format!("{}\n", serde_json::to_string(&action).unwrap()).as_bytes()).unwrap();
 
     if let Action::Flags(_) = &action {
-        start_listening(stream)
-    } else {
-        return
+        let err = start_listening(stream);
+        println!(r#",[{{"name":"error","long_text":"disconnected: {}","short_text":"disconnected","markup":"none"}}]"#, err);
+        std::thread::park();
     }
-
-    println!(r#"[{{name: "error", long_text: "disconnected", short_text: "disconnected"}}]"#)
 }
 
-fn start_listening(stream: TcpStream) {
+fn start_listening(stream: TcpStream) -> String {
     // create a buffered stream, which we'll read from line by line for status outputs
     let mut buf_stream = io::BufReader::new(stream);
 
@@ -53,11 +51,11 @@ fn start_listening(stream: TcpStream) {
         match buf_stream.read_line(&mut s) {
             Ok(n) => {
                 if n == 0 {
-                    return
+                    return "no bytes read".to_owned()
                 }
                 print!("{}", s);
             }
-            Err(_) => return,
+            Err(e) => return format!("error: {}", e),
         }
     };
 }
