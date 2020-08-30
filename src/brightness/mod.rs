352c345fb096cc6184485babccae1887a6711b18
Harrison Thorne <harrisonthorne@protonmail.com>
2020-08-29 23:53:53 -0600

    brightness: Fix output

--- src/brightness/mod.rs
+++ src/brightness/mod.rs
@@ -2,11 +2,19 @@ use crate::errors::*;
 use crate::format::blocks::output::*;
 use crate::format::blocks::*;
 use crate::format::color::Color;
+use crate::format::Attention;
 use crate::utils;
 use std::path::PathBuf;
 
 const BASE_DIR: &str = "/sys/class/backlight/";
-const BRIGHTNESS_ICONS: [char; 6] = ['\u{F00DB}', '\u{F00DC}', '\u{F00DD}', '\u{F00DE}', '\u{F00DF}', '\u{F00E0}'];
+const BRIGHTNESS_ICONS: [char; 6] = [
+    '\u{F00DB}',
+    '\u{F00DC}',
+    '\u{F00DD}',
+    '\u{F00DE}',
+    '\u{F00DF}',
+    '\u{F00E0}',
+];
 
 /// BrightnessBlock is a block that contains device brightness information
 pub struct BrightnessBlock {
@@ -88,17 +96,18 @@ impl Block for BrightnessBlock {
 
         let percent = self.current_brightness * 100 / self.max_brightness;
         let icon = get_icon(percent as u32);
-        Some(BlockOutputContent::from(crate::format::bit::Bit {
-            text: format!("{}  {}%", icon, percent),
-            color: Some(Color::Secondary),
-            font: None,
+        Some(BlockOutputContent::Nice(NiceOutput {
+            icon,
+            primary_text: format!("{}%", percent),
+            secondary_text: None,
+            attention: Attention::Dim,
         }))
     }
 }
 
 fn get_icon(percentage: u32) -> char {
-    let index = (percentage as usize * BRIGHTNESS_ICONS.len() / 100)
-        .min(BRIGHTNESS_ICONS.len() - 1);
+    let index =
+        (percentage as usize * BRIGHTNESS_ICONS.len() / 100).min(BRIGHTNESS_ICONS.len() - 1);
 
     BRIGHTNESS_ICONS[index]
 }
