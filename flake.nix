c700fc52dcf49f4b8cd0b8c1b95c847d995147d9
municorn <municorn@musicaloft.com>
2021-12-21 02:56:39 -0600

    Add Nix flake <3

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
+
+        nativeBuildInputs = with pkgs; [ rustc cargo dbus pkg-config libressl ];
+        buildInputs = with pkgs; [ dbus alsa-lib ];
+
+        muse-status = naersk-lib.buildPackage {
+          pname = "muse-status";
+          root = builtins.path {
+            path = ./.;
+            name = "muse-status-src";
+          };
+          inherit nativeBuildInputs buildInputs;
+        };
+
+        muse-status-client-app = flake-utils.lib.mkApp {
+          name = "muse-status-client";
+          drv = muse-status;
+          exePath = "/bin/muse-status";
+        };
+
+        muse-status-daemon-app = flake-utils.lib.mkApp {
+          name = "muse-status-daemon";
+          drv = muse-status;
+          exePath = "/bin/muse-status-daemon";
+        };
+      in
+      rec {
+        # `nix build`
+        packages = { inherit muse-status; };
+        defaultPackage = muse-status;
+
+        # `nix run`
+        apps.muse-status = muse-status-client-app;
+        apps.muse-status-daemon = muse-status-daemon-app;
+
+        # `nix develop`
+        devShell =
+          let
+            inherit (pkgs) mkShell cargo-watch clippy rust-analyzer rustfmt;
+          in
+          mkShell {
+            nativeBuildInputs = nativeBuildInputs ++ buildInputs ++ [
+              cargo-watch
+              clippy
+              rust-analyzer
+              rustfmt
+            ];
+          };
+      }
+    );
+}
