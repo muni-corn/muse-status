39cb313f0b18a76c8e6d35907852772f909561d4
municorn <municorn@musicaloft.com>
2022-02-18 22:46:00 -0600

    flake: Change `ping` to `iputils`

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
           naersk-lib = naersk.lib."${system}";
 
           nativeBuildInputs = with pkgs; [ rustc cargo dbus pkg-config libressl ];
-          buildInputs = with pkgs; [ dbus pamixer alsa-utils ping ];
+          buildInputs = with pkgs; [ dbus pamixer alsa-utils iputils ];
 
           muse-status = naersk-lib.buildPackage {
             pname = "muse-status";
