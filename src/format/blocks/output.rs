a300fe38b31f4a2523d6fc7b3a50abfbe7ee0605
municorn <municorn@musicaloft.com>
2022-05-28 22:21:51 -0500

    format/blocks: Use `BlockText` to make pango output

--- src/format/blocks/output.rs
+++ src/format/blocks/output.rs
@@ -36,43 +36,9 @@ impl BlockOutput {
     /// Formats the output as a pango string. The first string returned is the full text including
     /// icon, primary text, and secondary text. The second string is the same but excludes the
     /// secondary text.
-    pub fn as_pango_strings(&self, f: &Formatter) -> (String, String) {
+    pub fn as_pango_strings(&self, f: &Formatter) -> (String, Option<String>) {
         let (primary_color, secondary_color) = self.attention.colors(f);
-
-        let icon_bit_opt = self.icon.map(|icon| Bit {
-            text: icon.to_string(),
-            color: Some(Color::Other(primary_color.clone())),
-            font: Some(f.icon_font.clone()),
-        });
-
-        let primary_bit = Bit {
-            text: self.primary_text.clone(),
-            color: Some(Color::Other(primary_color)),
-            font: None,
-        };
-
-        let secondary_bit_opt = self.secondary_text.as_ref().map(|t| Bit {
-            text: t.clone(),
-            color: Some(Color::Other(secondary_color)),
-            font: None,
-        });
-
-        let short_text = if let Some(icon_bit) = icon_bit_opt {
-            format!(
-                "{}  {}",
-                icon_bit.as_pango_string(f),
-                primary_bit.as_pango_string(f)
-            )
-        } else {
-            primary_bit.as_pango_string(f)
-        };
-
-        if let Some(secondary_bit) = secondary_bit_opt {
-            let full_text = format!("{}  {}", short_text, secondary_bit.as_pango_string(f));
-            (full_text, short_text)
-        } else {
-            (short_text.clone(), short_text)
-        }
+        self.text.to_pango_strings(f, primary_color, secondary_color)
     }
 }
 
